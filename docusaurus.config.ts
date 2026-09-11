8381f867da5eca4b72592b3c205d1a38e86afbb6
Mats Bjønnes <mats.bjonnes@gmail.com>
2026-09-12 00:40:34 +0200

    site (fix): Modify docusaurus base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://nuddel69.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Zephyr-Course/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
